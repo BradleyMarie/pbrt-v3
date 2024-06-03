024ae0ef2c077338c299cd85570640dce52b53fa
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2024-06-03 00:52:52 -0700

    Skip bump mapping if both du and dv are zero

--- src/core/material.cpp
+++ src/core/material.cpp
@@ -45,16 +45,17 @@ Material::~Material() {}
 
 void Material::Bump(const std::shared_ptr<Texture<Float>> &d,
                     SurfaceInteraction *si) {
+    // Skip bump mapping if both du and dv are zero
+    Float du = .5f * (std::abs(si->dudx) + std::abs(si->dudy));
+    Float dv = .5f * (std::abs(si->dvdx) + std::abs(si->dvdy));
+    if (du == 0.0 && dv == 0.0) {
+      return;
+    }
+
     // Compute offset positions and evaluate displacement texture
     SurfaceInteraction siEval = *si;
 
     // Shift _siEval_ _du_ in the $u$ direction
-    Float du = .5f * (std::abs(si->dudx) + std::abs(si->dudy));
-    // The most common reason for du to be zero is for ray that start from
-    // light sources, where no differentials are available. In this case,
-    // we try to choose a small enough du so that we still get a decently
-    // accurate bump value.
-    if (du == 0) du = .0005f;
     siEval.p = si->p + du * si->shading.dpdu;
     siEval.uv = si->uv + Vector2f(du, 0.f);
     siEval.n = Normalize((Normal3f)Cross(si->shading.dpdu, si->shading.dpdv) +
@@ -62,8 +63,6 @@ void Material::Bump(const std::shared_ptr<Texture<Float>> &d,
     Float uDisplace = d->Evaluate(siEval);
 
     // Shift _siEval_ _dv_ in the $v$ direction
-    Float dv = .5f * (std::abs(si->dvdx) + std::abs(si->dvdy));
-    if (dv == 0) dv = .0005f;
     siEval.p = si->p + dv * si->shading.dpdv;
     siEval.uv = si->uv + Vector2f(0.f, dv);
     siEval.n = Normalize((Normal3f)Cross(si->shading.dpdu, si->shading.dpdv) +
