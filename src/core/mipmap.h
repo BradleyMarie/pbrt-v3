80d6a52acab115cc5d5e348c1b710d9e7ceeada5
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2025-08-08 17:41:27 -0700

    Disable mipmapping

--- src/core/mipmap.h
+++ src/core/mipmap.h
@@ -38,12 +38,14 @@
 #ifndef PBRT_CORE_MIPMAP_H
 #define PBRT_CORE_MIPMAP_H
 
+#include <type_traits>
+
 // core/mipmap.h*
+#include "parallel.h"
 #include "pbrt.h"
 #include "spectrum.h"
-#include "texture.h"
 #include "stats.h"
-#include "parallel.h"
+#include "texture.h"
 
 namespace pbrt {
 
@@ -126,32 +128,35 @@ MIPMap<T>::MIPMap(const Point2i &res, const T *img, bool doTrilinear,
     if (!IsPowerOf2(resolution[0]) || !IsPowerOf2(resolution[1])) {
         // Resample image to power-of-two resolution
         Point2i resPow2(RoundUpPow2(resolution[0]), RoundUpPow2(resolution[1]));
-        LOG(INFO) << "Resampling MIPMap from " << resolution << " to " <<
-            resPow2 << ". Ratio= " << (Float(resPow2.x * resPow2.y) /
-                                       Float(resolution.x * resolution.y));
+        LOG(INFO) << "Resampling MIPMap from " << resolution << " to "
+                  << resPow2 << ". Ratio= "
+                  << (Float(resPow2.x * resPow2.y) /
+                      Float(resolution.x * resolution.y));
         // Resample image in $s$ direction
         std::unique_ptr<ResampleWeight[]> sWeights =
             resampleWeights(resolution[0], resPow2[0]);
         resampledImage.reset(new T[resPow2[0] * resPow2[1]]);
 
         // Apply _sWeights_ to zoom in $s$ direction
-        ParallelFor([&](int t) {
-            for (int s = 0; s < resPow2[0]; ++s) {
-                // Compute texel $(s,t)$ in $s$-zoomed image
-                resampledImage[t * resPow2[0] + s] = 0.f;
-                for (int j = 0; j < 4; ++j) {
-                    int origS = sWeights[s].firstTexel + j;
-                    if (wrapMode == ImageWrap::Repeat)
-                        origS = Mod(origS, resolution[0]);
-                    else if (wrapMode == ImageWrap::Clamp)
-                        origS = Clamp(origS, 0, resolution[0] - 1);
-                    if (origS >= 0 && origS < (int)resolution[0])
-                        resampledImage[t * resPow2[0] + s] +=
-                            sWeights[s].weight[j] *
-                            img[t * resolution[0] + origS];
+        ParallelFor(
+            [&](int t) {
+                for (int s = 0; s < resPow2[0]; ++s) {
+                    // Compute texel $(s,t)$ in $s$-zoomed image
+                    resampledImage[t * resPow2[0] + s] = 0.f;
+                    for (int j = 0; j < 4; ++j) {
+                        int origS = sWeights[s].firstTexel + j;
+                        if (wrapMode == ImageWrap::Repeat)
+                            origS = Mod(origS, resolution[0]);
+                        else if (wrapMode == ImageWrap::Clamp)
+                            origS = Clamp(origS, 0, resolution[0] - 1);
+                        if (origS >= 0 && origS < (int)resolution[0])
+                            resampledImage[t * resPow2[0] + s] +=
+                                sWeights[s].weight[j] *
+                                img[t * resolution[0] + origS];
+                    }
                 }
-            }
-        }, resolution[1], 16);
+            },
+            resolution[1], 16);
 
         // Resample image in $t$ direction
         std::unique_ptr<ResampleWeight[]> tWeights =
@@ -160,24 +165,27 @@ MIPMap<T>::MIPMap(const Point2i &res, const T *img, bool doTrilinear,
         int nThreads = MaxThreadIndex();
         for (int i = 0; i < nThreads; ++i)
             resampleBufs.push_back(new T[resPow2[1]]);
-        ParallelFor([&](int s) {
-            T *workData = resampleBufs[ThreadIndex];
-            for (int t = 0; t < resPow2[1]; ++t) {
-                workData[t] = 0.f;
-                for (int j = 0; j < 4; ++j) {
-                    int offset = tWeights[t].firstTexel + j;
-                    if (wrapMode == ImageWrap::Repeat)
-                        offset = Mod(offset, resolution[1]);
-                    else if (wrapMode == ImageWrap::Clamp)
-                        offset = Clamp(offset, 0, (int)resolution[1] - 1);
-                    if (offset >= 0 && offset < (int)resolution[1])
-                        workData[t] += tWeights[t].weight[j] *
-                                       resampledImage[offset * resPow2[0] + s];
+        ParallelFor(
+            [&](int s) {
+                T *workData = resampleBufs[ThreadIndex];
+                for (int t = 0; t < resPow2[1]; ++t) {
+                    workData[t] = 0.f;
+                    for (int j = 0; j < 4; ++j) {
+                        int offset = tWeights[t].firstTexel + j;
+                        if (wrapMode == ImageWrap::Repeat)
+                            offset = Mod(offset, resolution[1]);
+                        else if (wrapMode == ImageWrap::Clamp)
+                            offset = Clamp(offset, 0, (int)resolution[1] - 1);
+                        if (offset >= 0 && offset < (int)resolution[1])
+                            workData[t] +=
+                                tWeights[t].weight[j] *
+                                resampledImage[offset * resPow2[0] + s];
+                    }
                 }
-            }
-            for (int t = 0; t < resPow2[1]; ++t)
-                resampledImage[t * resPow2[0] + s] = clamp(workData[t]);
-        }, resPow2[0], 32);
+                for (int t = 0; t < resPow2[1]; ++t)
+                    resampledImage[t * resPow2[0] + s] = clamp(workData[t]);
+            },
+            resPow2[0], 32);
         for (auto ptr : resampleBufs) delete[] ptr;
         resolution = resPow2;
     }
@@ -196,14 +204,16 @@ MIPMap<T>::MIPMap(const Point2i &res, const T *img, bool doTrilinear,
         pyramid[i].reset(new BlockedArray<T>(sRes, tRes));
 
         // Filter four texels from finer level of pyramid
-        ParallelFor([&](int t) {
-            for (int s = 0; s < sRes; ++s)
-                (*pyramid[i])(s, t) =
-                    .25f * (Texel(i - 1, 2 * s, 2 * t) +
-                            Texel(i - 1, 2 * s + 1, 2 * t) +
-                            Texel(i - 1, 2 * s, 2 * t + 1) +
-                            Texel(i - 1, 2 * s + 1, 2 * t + 1));
-        }, tRes, 16);
+        ParallelFor(
+            [&](int t) {
+                for (int s = 0; s < sRes; ++s)
+                    (*pyramid[i])(s, t) =
+                        .25f * (Texel(i - 1, 2 * s, 2 * t) +
+                                Texel(i - 1, 2 * s + 1, 2 * t) +
+                                Texel(i - 1, 2 * s, 2 * t + 1) +
+                                Texel(i - 1, 2 * s + 1, 2 * t + 1));
+            },
+            tRes, 16);
     }
 
     // Initialize EWA filter weights if needed
@@ -245,111 +255,37 @@ template <typename T>
 T MIPMap<T>::Lookup(const Point2f &st, Float width) const {
     ++nTrilerpLookups;
     ProfilePhase p(Prof::TexFiltTrilerp);
-    // Compute MIPMap level for trilinear filtering
-    Float level = Levels() - 1 + Log2(std::max(width, (Float)1e-8));
-
-    // Perform trilinear interpolation at appropriate MIPMap level
-    if (level < 0)
-        return triangle(0, st);
-    else if (level >= Levels() - 1)
-        return Texel(Levels() - 1, 0, 0);
-    else {
-        int iLevel = std::floor(level);
-        Float delta = level - iLevel;
-        return Lerp(delta, triangle(iLevel, st), triangle(iLevel + 1, st));
-    }
+    return triangle(0, st);
 }
 
 template <typename T>
 T MIPMap<T>::triangle(int level, const Point2f &st) const {
-    level = Clamp(level, 0, Levels() - 1);
-    Float s = st[0] * pyramid[level]->uSize() - 0.5f;
-    Float t = st[1] * pyramid[level]->vSize() - 0.5f;
-    int s0 = std::floor(s), t0 = std::floor(t);
-    Float ds = s - s0, dt = t - t0;
-    return (1 - ds) * (1 - dt) * Texel(level, s0, t0) +
-           (1 - ds) * dt * Texel(level, s0, t0 + 1) +
-           ds * (1 - dt) * Texel(level, s0 + 1, t0) +
-           ds * dt * Texel(level, s0 + 1, t0 + 1);
+    if (std::is_same<T, float>::value) {
+        level = Clamp(level, 0, Levels() - 1);
+        Float s = st[0] * pyramid[level]->uSize() - 0.5f;
+        Float t = st[1] * pyramid[level]->vSize() - 0.5f;
+        int s0 = std::floor(s), t0 = std::floor(t);
+        Float ds = s - s0, dt = t - t0;
+        return (1 - ds) * (1 - dt) * Texel(level, s0, t0) +
+            (1 - ds) * dt * Texel(level, s0, t0 + 1) +
+            ds * (1 - dt) * Texel(level, s0 + 1, t0) +
+            ds * dt * Texel(level, s0 + 1, t0 + 1);
+    } else {
+        Float s = st[0] * pyramid[level]->uSize();
+        Float t = st[1] * pyramid[level]->vSize();
+        int s0 = std::floor(s), t0 = std::floor(t);
+        return Texel(0, s0, t0);
+    }
 }
 
 template <typename T>
 T MIPMap<T>::Lookup(const Point2f &st, Vector2f dst0, Vector2f dst1) const {
-    if (doTrilinear) {
-        Float width = std::max(std::max(std::abs(dst0[0]), std::abs(dst0[1])),
-                               std::max(std::abs(dst1[0]), std::abs(dst1[1])));
-        return Lookup(st, width);
-    }
-    ++nEWALookups;
-    ProfilePhase p(Prof::TexFiltEWA);
-    // Compute ellipse minor and major axes
-    if (dst0.LengthSquared() < dst1.LengthSquared()) std::swap(dst0, dst1);
-    Float majorLength = dst0.Length();
-    Float minorLength = dst1.Length();
-
-    // Clamp ellipse eccentricity if too large
-    if (minorLength * maxAnisotropy < majorLength && minorLength > 0) {
-        Float scale = majorLength / (minorLength * maxAnisotropy);
-        dst1 *= scale;
-        minorLength *= scale;
-    }
-    if (minorLength == 0) return triangle(0, st);
-
-    // Choose level of detail for EWA lookup and perform EWA filtering
-    Float lod = std::max((Float)0, Levels() - (Float)1 + Log2(minorLength));
-    int ilod = std::floor(lod);
-    return Lerp(lod - ilod, EWA(ilod, st, dst0, dst1),
-                EWA(ilod + 1, st, dst0, dst1));
+    return triangle(0, st);
 }
 
 template <typename T>
 T MIPMap<T>::EWA(int level, Point2f st, Vector2f dst0, Vector2f dst1) const {
-    if (level >= Levels()) return Texel(Levels() - 1, 0, 0);
-    // Convert EWA coordinates to appropriate scale for level
-    st[0] = st[0] * pyramid[level]->uSize() - 0.5f;
-    st[1] = st[1] * pyramid[level]->vSize() - 0.5f;
-    dst0[0] *= pyramid[level]->uSize();
-    dst0[1] *= pyramid[level]->vSize();
-    dst1[0] *= pyramid[level]->uSize();
-    dst1[1] *= pyramid[level]->vSize();
-
-    // Compute ellipse coefficients to bound EWA filter region
-    Float A = dst0[1] * dst0[1] + dst1[1] * dst1[1] + 1;
-    Float B = -2 * (dst0[0] * dst0[1] + dst1[0] * dst1[1]);
-    Float C = dst0[0] * dst0[0] + dst1[0] * dst1[0] + 1;
-    Float invF = 1 / (A * C - B * B * 0.25f);
-    A *= invF;
-    B *= invF;
-    C *= invF;
-
-    // Compute the ellipse's $(s,t)$ bounding box in texture space
-    Float det = -B * B + 4 * A * C;
-    Float invDet = 1 / det;
-    Float uSqrt = std::sqrt(det * C), vSqrt = std::sqrt(A * det);
-    int s0 = std::ceil(st[0] - 2 * invDet * uSqrt);
-    int s1 = std::floor(st[0] + 2 * invDet * uSqrt);
-    int t0 = std::ceil(st[1] - 2 * invDet * vSqrt);
-    int t1 = std::floor(st[1] + 2 * invDet * vSqrt);
-
-    // Scan over ellipse bound and compute quadratic equation
-    T sum(0.f);
-    Float sumWts = 0;
-    for (int it = t0; it <= t1; ++it) {
-        Float tt = it - st[1];
-        for (int is = s0; is <= s1; ++is) {
-            Float ss = is - st[0];
-            // Compute squared radius and filter texel if inside ellipse
-            Float r2 = A * ss * ss + B * ss * tt + C * tt * tt;
-            if (r2 < 1) {
-                int index =
-                    std::min((int)(r2 * WeightLUTSize), WeightLUTSize - 1);
-                Float weight = weightLut[index];
-                sum += Texel(level, is, it) * weight;
-                sumWts += weight;
-            }
-        }
-    }
-    return sum / sumWts;
+    return triangle(0, st);
 }
 
 template <typename T>
